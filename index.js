31871fd786241ec47bf01e1d22f718649f5400fd
Mark Bond <mark.bond@dominos.com.au>
2019-05-21 13:58:28 +1000

    Check environment variables

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8180
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var html = `<html>
+            <body>
+                <div>Elasticache Endpoint: ${process.env.ELASTICACHE_ENDPOINT}</div>
+                <div>Elasticache Port: ${process.env.ELASTICACHE_PORT}</div>
+            </body>`
+
+app.get('/', (req, res) => res.send(html))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/*
+var redisConfig = {
+    port: process.env.ELASTICACHE_PORT,
+    host: process.env.ELASTICACHE_ENDPOINT
+};
+
+var store = new LaunchDarkly.RedisFeatureStore(redisConfig);
+
+
+
+var ldConfig = {
+    feature_store: store
+};
+var ldClient = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY, ldConfig);
+
+ldClient.once('ready', () => {
+    ldClient.close();
+});
+*/
