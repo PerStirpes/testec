104f7772a562599cc6ae75ebf7a5cd4d198ac2cf
Mark Bond <mark.bond@dominos.com.au>
2019-05-23 11:44:09 +1000

    Fixed with LaunchDarkly's support ticket info

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 8081
 var LaunchDarkly = require('launchdarkly-node-server-sdk');
-var winston = require('winston');
+var redisOptions = {port: process.env.REDIS_PORT, host: process.env.REDIS_ENDPOINT}
+var store = new LaunchDarkly.RedisFeatureStore(redisOptions)
+var config = {feature_store: store, offline: true}
 
-var redisConfig = {
-    //url: `redis://${process.env.ELASTICACHE_ENDPOINT}:${process.env.ELASTICACHE_PORT}`,
-    port: process.env.ELASTICACHE_PORT,
-    host: process.env.ELASTICACHE_ENDPOINT
-};
+// SDK key is not required when in offline mode
+var ldClient = LaunchDarkly.init(null, {config})
 
-var store = new LaunchDarkly.RedisFeatureStore(redisConfig);
+ldClient.on('ready', function() {
+ console.log("The LaunchDarkly SDK is now initialized with flag configurations stored in redis.")
 
-logger =
-  new winston.Logger({
-    level: "debug",
-    transports: [
-      new(winston.transports.Console)(),
-    ]
-  }
-);
+  var customerLoginEnabled = false;
+ 
+  ldClient.variation("customer-login-enabled", {}, false, function(err, customerLoginEnabled) {
+    console.log(`customerLoginEnabled = ${customerLoginEnabled}}`)
+  });
 
-var ldConfig = {
-    featureStore: store,
-    useLdd: true,
-    logger: logger
-};
-
-var ldClient = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY, ldConfig);
-
-var customerLoginEnabled = false;
-var customerFeatureHtml = '';
-
-ldClient.once('ready', () => {
-
-    ldClient.variation("customer-login-enabled", {}, false, function(err, customerLoginEnabled) {
-        customerFeatureHtml = `customerLoginEnabled: ${customerLoginEnabled}`
-
-        var html = `<html>
-        <body>
-            <div>Elasticache Endpoint: ${process.env.ELASTICACHE_ENDPOINT}</div>
-            <div>Elasticache Port: ${process.env.ELASTICACHE_PORT}</div>
-            <div>${customerFeatureHtml}</div>
-        </body>`
-
-        app.get('/', (req, res) => res.send(html))
-
-    });
-
-    ldClient.close();
-});
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+ ldClient.close() 
+})
